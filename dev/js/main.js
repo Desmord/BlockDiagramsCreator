c5d91d5d504b0355a2d79f28c574a7653a5c284d
MikolajChojnacki <mikolaj.chojnacki@wp.pl>
2020-04-17 19:06:43 +0200

    Button style without icon

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,4 +1,16 @@
 import '../css/main.css';
 
-
 console.log(`program hej`);
+
+window.addEventListener('DOMContentLoaded', (event) => {
+
+    let element = document.querySelector(`.button:nth-of-type(2)`);
+    let element2 = document.querySelector(`.menu`);
+
+
+    console.log(element);
+    console.log(element2);
+
+
+});
+
