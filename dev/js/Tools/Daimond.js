bce33e289f13dae67de0b8ebe33b6b5ab5518698
MikolajChojnacki <mikolaj.chojnacki@wp.pl>
2020-05-03 15:34:16 +0200

    Add eraser

--- dev/js/Tools/Daimond.js
+++ dev/js/Tools/Daimond.js
@@ -1,21 +1,111 @@
+const CanvasContainerElements = require(`../DOMElements.js`).CanvasContainerElements;
+
 class Daimond {
-    constructor() {
-        this.name = `diamond`
+    constructor(cavansStateManager) {
+        this.canvas = CanvasContainerElements.canvas;
+        this.context = this.canvas.getContext(`2d`);
+        this.cavansStateManager = cavansStateManager;
+        this.name = `diamond`;
+        this.color = `rgba(0,0,0,1)`;
+        this.lineWidth = `5`;
+        this.isDrawing = false;
+        this.startPoint = {
+            x: 0,
+            y: 0
+        }
+    }
+    onMouseDown(e) {
+        this.startPoint.x = this.getMouseHorizontalPosition(e);
+        this.startPoint.y = this.getMouseVerticalPosition(e);
+        this.isDrawing = true;
     }
 
-    onMouseDown() { }
-    onMouseMove() { }
-    onMouseUp() { }
-    stopDrawing(){}
+    onMouseMove(e) {
+        if (this.isDrawing) {
+            this.clearCanvas();
+            this.restoreState();
+            this.drawDaimond(e)
+        }
+    }
+
+    onMouseUp(e) {
+        this.isDrawing = false;
+        this.drawDaimond(e);
+        this.cavansStateManager.addState();
+    }
+
+    drawDaimond(e) {
+        window.requestAnimationFrame(() => {
+            this.context.strokeStyle = this.color;
+            this.context.lineWidth = this.lineWidth;
+
+            // left - top
+            this.context.beginPath();
+            this.context.moveTo(this.startPoint.x + (parseInt(this.lineWidth / 2)), this.startPoint.y + ((this.getMouseVerticalPosition(e) - this.startPoint.y) / 2));
+            this.context.lineTo(this.startPoint.x + (parseInt(this.lineWidth / 2)) + ((this.getMouseHorizontalPosition(e) - this.startPoint.x) / 2), this.startPoint.y);
+            this.context.stroke();
+            // /left - bottom
+            this.context.beginPath();
+            this.context.moveTo(this.startPoint.x, this.startPoint.y + ((this.getMouseVerticalPosition(e) - this.startPoint.y) / 2));
+            this.context.lineTo(this.startPoint.x + ((this.getMouseHorizontalPosition(e) - this.startPoint.x) / 2), this.getMouseVerticalPosition(e));
+            this.context.stroke();
+            // right - top
+            this.context.beginPath();
+            this.context.moveTo(this.startPoint.x + ((this.getMouseHorizontalPosition(e) - this.startPoint.x) / 2), this.startPoint.y);
+            this.context.lineTo(this.getMouseHorizontalPosition(e), this.startPoint.y + ((this.getMouseVerticalPosition(e) - this.startPoint.y) / 2));
+            this.context.stroke();
+            // right - bottom
+            this.context.beginPath();
+            this.context.moveTo(this.startPoint.x - (parseInt(this.lineWidth / 2)) + ((this.getMouseHorizontalPosition(e) - this.startPoint.x) / 2), this.getMouseVerticalPosition(e));
+            this.context.lineTo(this.getMouseHorizontalPosition(e) - (parseInt(this.lineWidth / 2)), this.startPoint.y + ((this.getMouseVerticalPosition(e) - this.startPoint.y) / 2));
+            this.context.stroke();
+        })
+    }
+
+    stopDrawing() {
+        this.isDrawing = false;
+        this.cavansStateManager.addState();
+    }
+
+    clearCanvas() {
+        let color = this.color;
+        this.context.fillStyle = `rgb(255,255,255)`;
+        this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+        this.context.fillStyle = color;
+    }
+
+    restoreState() {
+        this.context.drawImage(this.cavansStateManager.getLastState(), 0, 0)
+    }
+
+    getMouseHorizontalPosition(e) {
+        return e.clientX + this.canvas.parentElement.scrollLeft - this.getCanvasLeftMargin(e)
+    }
+
+    getCanvasLeftMargin(e) {
+        const padding = 30;
+        return e.target.parentElement.offsetLeft +
+            e.target.parentElement.parentElement.offsetLeft + padding
+    }
+
+    getMouseVerticalPosition(e) {
+        return e.clientY + this.canvas.parentElement.scrollTop - this.getCanvasTopMargin(e)
+    }
+
+    getCanvasTopMargin(e) {
+        return e.target.parentElement.offsetTop +
+            e.target.parentElement.parentElement.offsetTop
+    }
 
     updateColor(newColor) {
-        console.log(`${this.name} color - ${newColor}`);
+        this.color = newColor;
 
     }
-    updateFontSize() { }
     updateLineWidth(newLineWidth) {
-        console.log(`${this.name} odswierzam szerokosc lini ${newLineWidth}`);
-     }
+        this.lineWidth = newLineWidth
+    }
+
+    updateFontSize() { }
 }
 
 
--- dev/js/Tools/Eraser.js
+++ dev/js/Tools/Eraser.js
@@ -1,21 +1,140 @@
+const CanvasContainerElements = require(`../DOMElements.js`).CanvasContainerElements;
+
 class Eraser {
-    constructor() {
-        this.name =`eraser`
+    constructor(cavansStateManager) {
+        this.name = `eraser`
+        this.canvas = CanvasContainerElements.canvas;
+        this.context = this.canvas.getContext(`2d`);
+        this.cavansStateManager = cavansStateManager;
+        this.color = `rgba(0,0,0,1)`;
+        this.lineWidth = `5`;
+        this.isDrawing = false;
+        this.startPoint = {
+            x: 0,
+            y: 0
+        }
     }
 
-    onMouseDown() { }
-    onMouseMove() { }
-    onMouseUp() { }
-    stopDrawing(){}
+
+    onMouseDown(e) {
+        this.startPoint.x = this.getMouseHorizontalPosition(e);
+        this.startPoint.y = this.getMouseVerticalPosition(e);
+        this.isDrawing = true;
+        // this.clearCanvas();
+        // this.restoreState();
+    }
+
+    onMouseMove(e) {
+        this.startPoint.x = this.getMouseHorizontalPosition(e);
+        this.startPoint.y = this.getMouseVerticalPosition(e);
+
+        if (this.isDrawing) {
+            this.erase(e)
+        } else {
+            this.clearCanvas();
+            this.restoreState();
+            this.drawEraserBorders(e);
+
+        }
+        this.cavansStateManager.addState();
+
+    }
+
+    onMouseUp(e) {
+        this.isDrawing = false;
+    }
+
+
+    erase(e) {
+        window.requestAnimationFrame(() => {
+            this.context.fillStyle = `rgb(255,255,255)`;
+            this.context.fillRect(
+                this.startPoint.x - (this.lineWidth * 3) - this.lineWidth,
+                this.startPoint.y - (this.lineWidth * 3) - this.lineWidth,
+                parseInt(this.lineWidth * 6) + parseInt(this.lineWidth * 1.2),
+                parseInt(this.lineWidth * 6) + parseInt(this.lineWidth * 1.2)
+            );
+        })
+    }
+
+    /**
+     * Size of the square border depends on lineWidth
+     * @param {event} e
+     */
+    drawEraserBorders(e) {
+        window.requestAnimationFrame(() => {
+            this.context.strokeStyle = `rgb(90,90,90)`;
+            this.context.lineWidth = 2;
+
+            // left
+            this.context.beginPath();
+            this.context.moveTo(this.startPoint.x - (this.lineWidth * 3), this.startPoint.y + (this.lineWidth * 3));
+            this.context.lineTo(this.startPoint.x - (this.lineWidth * 3), this.getMouseVerticalPosition(e) - (this.lineWidth * 3));
+            this.context.stroke();
+            //top
+            this.context.beginPath();
+            this.context.moveTo(this.startPoint.x - (this.lineWidth * 3), this.startPoint.y - (this.lineWidth * 3));
+            this.context.lineTo(this.getMouseHorizontalPosition(e) + (this.lineWidth * 3), this.startPoint.y - (this.lineWidth * 3));
+            this.context.stroke();
+            // right
+            this.context.beginPath();
+            this.context.moveTo(this.getMouseHorizontalPosition(e) + (this.lineWidth * 3), this.startPoint.y + (this.lineWidth * 3));
+            this.context.lineTo(this.getMouseHorizontalPosition(e) + (this.lineWidth * 3), this.getMouseVerticalPosition(e) - (this.lineWidth * 3));
+            this.context.stroke();
+            // bottom
+            this.context.beginPath();
+            this.context.moveTo(this.startPoint.x - (this.lineWidth * 3), this.getMouseVerticalPosition(e) + (this.lineWidth * 3));
+            this.context.lineTo(this.getMouseHorizontalPosition(e) + (this.lineWidth * 3), this.getMouseVerticalPosition(e) + (this.lineWidth * 3));
+            this.context.stroke();
+
+        })
+    }
+
+    stopDrawing() {
+        this.isDrawing = false;
+        this.restoreState();
+        this.cavansStateManager.addState();
+    }
+
+    clearCanvas() {
+        let color = this.color;
+        this.context.fillStyle = `rgb(255,255,255)`;
+        this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+        this.context.fillStyle = color;
+    }
+
+    restoreState() {
+        this.context.drawImage(this.cavansStateManager.getLastState(), 0, 0)
+    }
+
+    getMouseHorizontalPosition(e) {
+        return e.clientX + this.canvas.parentElement.scrollLeft - this.getCanvasLeftMargin(e)
+    }
+
+    getCanvasLeftMargin(e) {
+        const padding = 30;
+        return e.target.parentElement.offsetLeft +
+            e.target.parentElement.parentElement.offsetLeft + padding
+    }
+
+    getMouseVerticalPosition(e) {
+        return e.clientY + this.canvas.parentElement.scrollTop - this.getCanvasTopMargin(e)
+    }
+
+    getCanvasTopMargin(e) {
+        return e.target.parentElement.offsetTop +
+            e.target.parentElement.parentElement.offsetTop
+    }
 
     updateColor(newColor) {
-        console.log(`${this.name} color - ${newColor}`);
+        this.color = newColor;
 
-     }
-    updateFontSize() { }
-    updateLineWidth(newLineWidth) {
-        console.log(`${this.name} odswierzam szerokosc lini ${newLineWidth}`);
     }
+    updateLineWidth(newLineWidth) {
+        this.lineWidth = newLineWidth
+    }
+
+    updateFontSize() { }
 }
 
 
